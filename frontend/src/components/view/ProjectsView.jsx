9fcfc10055e8ec1d42fa09f00063eee7566ae9da
Lee3m <lem.abairo@gmail.com>
2024-10-12 00:56:27 +0100

    added projects from backend

--- frontend/src/components/view/ProjectsView.jsx
+++ frontend/src/components/view/ProjectsView.jsx
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../ui/Card";
+import axios from "axios";
 
 const ProjectsView = () => {
+  // Initialization --------------------------
+  // State -----------------------------------
+  const [projects, setProjects] = useState([]);
+  // Handlers --------------------------------
+
+  const fetchProjects = async () => {
+    try {
+      const response = await axios.get("http://localhost:5000/projects");
+      if (Array.isArray(response.data.data)) {
+        setProjects(response.data.data);
+      } else {
+        console.error("API response data is not an array");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProjects();
+  }, []);
+  // View ------------------------------------
+
   return (
     <div className="bg-slate-300">
       <h1 className="text-7xl p-5 flex justify-center items-center mb-5">
         Projects
       </h1>
-      <div className=" h-screen grid grid-cols-3 gap-3">
-        <Card title="Project 1" />
-        <Card title="Project 2" />
-        <Card title="Project 3" />
+      <div className="h-screen grid grid-cols-3 gap-3">
+        {projects.map((project, index) => (
+          <Card key={index} title={project.title} />
+        ))}
       </div>
     </div>
   );
